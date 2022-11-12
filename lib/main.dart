82d1a6582ecc88a0b767b4f845d06de658360c85
Nikita Mishkov <mishkov.dd@gmail.com>
2022-11-12 19:03:20 +0300

    Fix mistake

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
           return const Trisemus();
         },
         Vizhiner.route: (context) {
-          return const c();
+          return const Vizhiner();
         },
       },
     );
